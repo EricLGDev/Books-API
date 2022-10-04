2ad7f134a440201cfc2c95138b8c6426031a09c2
Eric <ericlg.link@gmail.com>
2022-10-04 04:40:40 -0700

    More server set up

--- server.js
+++ server.js
@@ -2,3 +2,23 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
+
+// CONFIG
+require('dotenv').config()
+const PORT = process.env.PORT
+
+// MIDDLEWARE
+app.use(express.json())
+
+// ROOT INDEX ROUTE
+app.get('/', (req, res) => {
+    res.send('Books Api')
+})
+
+// CONTROLLER
+const booksController = require('./controllers/booksController')
+
+// LISTEN
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`)
+})
